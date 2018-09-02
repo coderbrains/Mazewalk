e81c3d872a1fe83a670dce539c996b414dc47091
Sachin V <sachin.murthy97@gmail.com>
2018-09-02 17:24:28 +0530

    Mazewalk
    Mazewalk is a simple Virtual Reality Game.

---
+++ mazewalk/Assets/aw.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class aw : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    public float speed = 7;
+
+    void Update()
+    {
+        transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+    }
+    public void OnTriggerEnter(Collider collide)
+    {
+        if(collide.gameObject.CompareTag("Trophytag"))
+        {
+            SceneManager.LoadScene("endscene");
+        }
+    }
+}
